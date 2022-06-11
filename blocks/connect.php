03b3e72df7e2d394fcc0fea7f0dc170bc15fb616
Dmitrybrv18 <dmitrybrv@gmail.com>
2022-06-12 00:08:30 +0300

    first rep

---
+++ blocks/connect.php
@@ -0,0 +1,4 @@
+<?php
+$mysql = new mysqli('localhost', 'd105846_dbuser', '=rXDzyjyw!ikJo~_', 'd105846_db');
+
+?>
---
+++ validation-form/auth.php
@@ -0,0 +1,30 @@
+<?php
+$login = filter_var(trim($_POST['login']),
+FILTER_SANITIZE_STRING);
+$pass = filter_var(trim($_POST['pass']),
+FILTER_SANITIZE_STRING);
+
+$pass = md5($pass."ewfwef9798");
+
+require "../blocks/connect.php";
+
+
+    $result = $mysql->query("SELECT * FROM `users` WHERE `login` = '$login' AND `pass` = '$pass'");
+    $user = $result->fetch_assoc();
+    if(empty($user) or count($user) == 0) {
+        echo "Такой пользователь не найден!";
+        exit();
+    }
+
+    setcookie('user', $user['name'], time() + 3600 * 24 * 31, "/");
+
+    // print_r($user);
+    // exit();
+
+
+
+    $mysql->close();
+
+    header('Location: /');
+
+?>
